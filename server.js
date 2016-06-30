f57e0665b99e469df1f80ae1fb98dd6d1a7ff842
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-06-30 19:33:57 +0800

    created server script

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('static'));
+
+var pages = {
+   '/': '/index.html',
+}
+
+for (page in pages) {
+   app.get(page, function (req, res) {
+      res.sendFile( __dirname + pages[page] );
+   });
+}
+
+
+var server = app.listen(9000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
